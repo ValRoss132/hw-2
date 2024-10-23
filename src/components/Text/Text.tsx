a3ae2b9a1413783ea586a48f8fba4adbe1bad42f
ValRoss132 <valross1321@gmail.com>
2024-10-23 22:50:53 +0300

    Text component created

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,22 +1,40 @@
-import * as React from 'react'
+import * as React from 'react';
 
 export type TextProps = {
-    /** Дополнительный класс */
-    className?: string;
-    /** Стиль отображения */
-    view?: 'title' | 'button' | 'p-20' | 'p-18' | 'p-16' | 'p-14';
-    /** Html-тег */
-    tag?:  'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'div' | 'p' | 'span';
-    /** Начертание шрифта */
-    weight?: 'normal' | 'medium' | 'bold';
-    /** Контент */
-    children: React.ReactNode;
-    /** Цвет */
-    color?: 'primary' | 'secondary' | 'accent';
-    /** Максимальное кол-во строк */
-    maxLines?: number;
+	/** Дополнительный класс */
+	className?: string;
+	/** Стиль отображения */
+	view?: 'title' | 'button' | 'p-20' | 'p-18' | 'p-16' | 'p-14';
+	/** Html-тег */
+	tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'div' | 'p' | 'span';
+	/** Начертание шрифта */
+	weight?: 'normal' | 'medium' | 'bold';
+	/** Контент */
+	children: React.ReactNode;
+	/** Цвет */
+	color?: 'primary' | 'secondary' | 'accent';
+	/** Максимальное кол-во строк */
+	maxLines?: number;
 };
 
-const Text: React.FC<TextProps> = () => null
+const Text: React.FC<TextProps> = ({
+	className,
+	view,
+	tag = 'p',
+	weight,
+	children,
+	color,
+	maxLines,
+}) => {
+	const Component = tag;
+	return (
+		<Component
+			className={`text ${className} ${view} ${weight} ${color}
+						${maxLines ? `style='max-lines:${maxLines}'` : ''}`}
+		>
+			{children}
+		</Component>
+	);
+};
 
 export default Text;
