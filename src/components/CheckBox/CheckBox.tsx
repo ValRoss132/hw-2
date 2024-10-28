470314abbdd6a2e92ba555b0f9de14fc18389283
ValRoss132 <valross1321@gmail.com>
2024-10-28 19:16:33 +0300

    CheckBox component created

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CheckIcon from '../icons/CheckIcon';
+import './CheckBox.css';
+
+import classNames from 'classnames';
 
 export type CheckBoxProps = Omit<
-  React.InputHTMLAttributes<HTMLInputElement>,
-  'onChange'
+	React.InputHTMLAttributes<HTMLInputElement>,
+	'onChange'
 > & {
-  /** Вызывается при клике на чекбокс */
-  onChange: (checked: boolean) => void;
+	/** Вызывается при клике на чекбокс */
+	onChange: (checked: boolean) => void;
 };
 
-const CheckBox: React.FC<CheckBoxProps> = () => null;
+const CheckBox: React.FC<CheckBoxProps> = ({ checked, onChange, ...props }) => {
+	const classDisabled = props.disabled ? 'disabled' : '';
+	return (
+		<label className="check">
+			<div className={`check__box ${classDisabled}`}>
+				{checked && (
+					<CheckIcon
+						className={`check-icon`}
+						width={40}
+						height={40}
+					/>
+				)}
+			</div>
+			<input
+				className="check__input"
+				type="checkbox"
+				checked={checked}
+				onChange={(e) => onChange(e.target.checked)}
+				{...props}
+			/>
+		</label>
+	);
+};
 
 export default CheckBox;
